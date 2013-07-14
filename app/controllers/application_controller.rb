5d5424a8b2dc8d3ca80ca5234881a36ab31c5f75
Dan Addington <daniel.anthony.addington@gmail.com>
2013-07-14 19:40:34 +1000

    tweaking UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+  before_filter :get_groups
+
+def get_groups
+  @groups = Group.all
+end
 
 end
